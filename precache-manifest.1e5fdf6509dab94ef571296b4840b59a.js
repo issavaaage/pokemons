4c5e8c04095f058bbe214af715709625f3b5b42e
bartolemio <bartolemio@gmail.com>
2020-10-07 15:27:11 +0300

    Updates

---
+++ precache-manifest.1e5fdf6509dab94ef571296b4840b59a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab81f4f4ffcefa939d53a4535faafef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1019e2b34129fea79449",
+    "url": "/static/css/main.583dbdc2.chunk.css"
+  },
+  {
+    "revision": "9e452992859b3b860774",
+    "url": "/static/js/2.3fe9ccd5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3fe9ccd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1019e2b34129fea79449",
+    "url": "/static/js/main.686a13bc.chunk.js"
+  },
+  {
+    "revision": "a0151496095c21fc15e1",
+    "url": "/static/js/runtime-main.1dfec50e.js"
+  }
+]);
